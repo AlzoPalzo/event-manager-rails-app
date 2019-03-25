9450414732a8ff4fd42d93c6b97fdfbe0e2de144
github username <github email address>
2019-03-25 14:29:36 +0000

    user controller and pages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,47 @@
 class UsersController < ApplicationController
-    before_action :find_user, only: [:show]
+    before_action :find_user, only: [:show, :edit, :update]
 
+    def new
+        @user = User.new
+    end
+    
+    def create
+        user = User.new(user_params)
+        if user.valid?
+            user.save
+            redirect_to user
+        else
+            flash[:user_error] = user.errors.full_messages
+            redirect_to new_user_path       
+        end
+    end
+    
     def show
         
+    end
+
+    def edit
+
+    end
+    
+    def update
+        @user.update(user_params)
+        if @user.valid?
+            redirect_to @user
+            flash[:update_success] = "Profile successfully updated!"
+        else
+            flash[:user_error] = @user.errors.full_messages
+            redirect_to edit_user_path       
+        end
     end
 
     private
 
     def find_user
         @user = User.find(params[:id])
+    end
+
+    def user_params
+        params.require(:user).permit(:name, :age, :bio)
     end
 end
