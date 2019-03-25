b027f9dbb0f7e7223659111915b0ffe9591e19ab
Khang <khang.le27@gmail.com>
2019-03-25 11:32:43 +0000

    first show page for occasions

--- app/controllers/occasions_controller.rb
+++ app/controllers/occasions_controller.rb
@@ -1,2 +1,17 @@
 class OccasionsController < ApplicationController
+  before_action :find_occasion, only: [:show]
+
+  def show
+
+  end
+
+  private
+
+  def find_occasion
+    @occasion = Occasion.find(params[:id])
+  end
+
+  def occasion_params
+    params.require(:occasion).permit(:name, :user_id, :date_time, :description)
+  end
 end
--- app/models/occasion.rb
+++ app/models/occasion.rb
@@ -2,4 +2,6 @@ class Occasion < ApplicationRecord
     has_many :user_occasions
     has_many :users, through: :user_occasions
     has_many :messages
+    has_many :occasion_locations
+    has_many :locations, through: :occasion_locations
 end
